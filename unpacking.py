f0ae45533242c938bd33b6a67e26078b6c0c70d2
lealinin <lea.linin@gmail.com>
2020-03-21 16:35:28 -0700

    Intro to Python's unpacking feature

---
+++ unpacking.py
@@ -0,0 +1,11 @@
+# We can grab these same items from a tuple or list:
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[3]
+# using the unpacking feature.
+
+coordinates = (1, 2, 3)
+x, y, z = coordinates
+print(x)
+print(y)
+print(z)
