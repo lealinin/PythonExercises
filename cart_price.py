df50787513aed7303609b42394e26ad3158113fc
lealinin <lea.linin@gmail.com>
2020-03-21 16:11:06 -0700

    Use for loop to iterate over a list and calculate the total price

---
+++ cart_price.py
@@ -0,0 +1,10 @@
+# Calculate the total price of all the items in a cart
+# The prices are included in a LIST
+prices = [20, 25, 13]
+total = 0
+
+for price in prices:
+    total += price
+print(f"Total: {total}")
+
+
