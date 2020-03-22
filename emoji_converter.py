843c763b313cd0310b7366625955a087c62698af
lealinin <lea.linin@gmail.com>
2020-03-21 20:11:48 -0700

    Create reusable function

--- emoji_converter.py
+++ emoji_converter.py
@@ -1,11 +1,16 @@
+def emoji_converter(message):
+    words = message.split(" ")
+    # ctrl cmd space to pull up emojis
+    emojis = {
+        ":)": "😀",
+        ":(": "😔"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
 message = input("> ")
-words = message.split(" ")
-# ctrl cmd space to pull up emojis
-emojis = {
-    ":)": "😀",
-    ":(": "😔"
-}
-output = ""
-for word in words:
-    output += emojis.get(word, word) + " "
-print(output)
+print(emoji_converter(message))
+
