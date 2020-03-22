d0cca333527700d54bffc90f399a018db00aa014
lealinin <lea.linin@gmail.com>
2020-03-21 20:19:34 -0700

    Intro to exceptions

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+try:
+    age = int(input('Age: '))
+    income = 20000
+    risk = income / age
+    print(age)
+except ZeroDivisionError:
+    print('Age cannot be 0')
+except ValueError:
+    print('Invalid value')
+
+
+
+
