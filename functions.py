952de61ad0264423cc0d72e41335e7df3a2a7512
lealinin <lea.linin@gmail.com>
2020-03-21 19:57:25 -0700

    Add examples for positional and keyword arguments

--- functions.py
+++ functions.py
@@ -4,6 +4,11 @@ def greet_user(first_name, last_name):
 
 
 print('Start')
+# positional arguments
 greet_user('John', 'Smith')
+# keyword arguments
+greet_user(last_name='Morrissey', first_name='Steven')
+# if you mix positional and keyword arguments
+greet_user('Jerry', last_name='Salinger')
 print('Finish')
 
